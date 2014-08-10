92a4927850d2ec2a8b6491256fe7e3f5d43d9c3a
Keith Morris <keith.morris@moxieusa.com>
2014-08-09 20:07:25 -0400

    switched through2 to through as through2 was not doing the trick of waiting for the task to end properly. Refactoring FTW.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 "use strict";
 var
 	gutil = require('gulp-util'),
 	exec = require('child_process').exec,
 	_ = require('lodash'),
 	PluginError = gutil.PluginError,
-	through = require('through2')
-	;
+	through = require('event-stream').through
+;
 
 var PLUGIN_NAME = 'gulp-nunit-runner';
 
@@ -16,6 +15,7 @@ var assemblies = [],
 
 function gulpNunitRunner(opts) {
 	options = opts;
+
 	if (!opts || !opts.command) {
 		throw new PluginError(PLUGIN_NAME, "Path to NUnit executable is required in options.command");
 	}
@@ -25,7 +25,7 @@ function gulpNunitRunner(opts) {
 	}
 
 	// Creating and return a stream through which each file will pass
-	return through.obj(transform, flush);
+	return through(transform, flush);
 }
 
 function parseSwitches(options) {
@@ -42,24 +42,42 @@ function parseSwitches(options) {
 	});
 }
 
-function transform(file, enc, callback) {
+function transform(file/*, enc, callback*/) {
 	assemblies.push(file.path);
 	this.push(file);
-	return callback();
 }
 
-function flush(callback) {
+function flush() {
+
+	var fail = _.bind(function () {
+		this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'NUnit tests failed'));
+	}, this);
+
+	var end = _.bind(function () {
+		this.emit('end');
+	}, this);
+
+	console.log(assemblies);
+
+	if (assemblies.length === 0) {
+		this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'assemblies are required.'));
+		return end();
+	}
+
 	var execute = [];
 	execute.push(options.command);
 	execute.push(switches.join(' '));
 	execute.push('"' + assemblies.join('" "') + '"');
 	execute = execute.join(' ');
 
-	var cp = exec(execute, function (err, stdout, stderr) {
+	var cp = exec(execute, function (err/*,stdout, stderr*/) {
 		if (err) {
-			return callback(err);
+			gutil.log(gutil.colors.red('NUnit tests failed.'));
+			return fail();
+		} else {
+			gutil.log(gutil.colors.cyan('NUnit tests passed'));
 		}
-		return callback();
+		return end();
 	});
 
 	cp.stdout.pipe(process.stdout);
